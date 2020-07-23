12d0611f0e1a2b11c286b7b534841c9f8d452752
Lucas C Correa <lucasccorrea@gmail.com>
2020-07-23 17:09:29 -0300

    change name lib, set to public package

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 
 export const config: Config = {
-  namespace: 'cp-modals',
+  namespace: 'webcomponent-manager-modals',
   plugins: [
     sass({
       injectGlobalPaths:[
